d152cbeee9baf52d1cba867d7f4a9b185435c6dc
Nathanael Bosch <nathanael.bosch@tum.de>
2018-06-29 16:13:18 +0200

    can now test with pytest

--- test_sample.py
+++ test_sample.py
@@ -2,16 +2,22 @@ import numpy as np
 from tools import *
 
 
+# Setup: Some simple example tensors
 dummy_tensor = np.array(range(2*3*4)).reshape(2, 3, 4)
 dummy_tensor.reshape(2, 3*4)
 dummy_matrix = np.array(range(2*3)).reshape(3, 2)
 
-print('Test matricization and refold')
-assert (refold(m_mode_matricisation(
-    dummy_tensor, 1), 1, dummy_tensor.shape) == dummy_tensor).all()
 
-print('Test m mode matrix mult')
-m_mode_matrix_multiplication(dummy_tensor, dummy_matrix, 1)
+def test_unfold_refold():
+    assert (refold(m_mode_matricisation(
+        dummy_tensor, 1), 1, dummy_tensor.shape) == dummy_tensor).all()
 
-print('Test full hosvd')
-assert frobenius_norm(full_hosvd(dummy_tensor)-dummy_tensor) < 1e-10
+
+def test_matrix_multiplication():
+    # Just checks if it runs to look for shape errors
+    m_mode_matrix_multiplication(dummy_tensor, dummy_matrix, 1)
+
+
+def test_full_hosvd():
+    # Full HOSVD should keep the tensor quite similar
+    assert frobenius_norm(full_hosvd(dummy_tensor)-dummy_tensor) < 1e-10
